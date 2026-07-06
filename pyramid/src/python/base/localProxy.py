76698daf58642c173b6580ce8f55828cba72c46d
Ubuntu <ubuntu@localhost.localdomain>
2026-07-06 23:32:39 +0800

    fix: remove pyramid module reference, build only java64

--- pyramid/src/python/base/localProxy.py
+++
@@ -1,8 +0,0 @@
-from com.github.catvod import Proxy as CatVodProxy
-
-class Proxy:
-    def getUrl(self, local):
-        return CatVodProxy.getUrl(local).replace('/proxy', '')
-
-    def getPort(self):
-        return CatVodProxy.getPort()
